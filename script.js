e99b53603d918a10618cd60b526c367e51d89c0d
JoshRGunter <150454492+JoshRGunter@users.noreply.github.com>
2025-04-27 14:52:38 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple scroll effect
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
